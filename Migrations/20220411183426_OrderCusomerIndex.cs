ffb5f6057db71d9f1d852ed9e494ef402c7a4d92
hNeshat-sh <h.neshat.sh@gmail.com>
2022-04-11 23:09:01 +0430

    order CalculateOrderPrice

---
+++ Migrations/20220411183426_OrderCusomerIndex.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharismaShop.Migrations
+{
+    public partial class OrderCusomerIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId_Id",
+                table: "Orders",
+                columns: new[] { "CustomerId", "Id" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CustomerId_Id",
+                table: "Orders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                unique: true);
+        }
+    }
+}
--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Linq;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using CharismaShop.Model;
 using CharismaShop.Dto;
-using AutoMapper;
 
 namespace CharismaShop.Services
 {
@@ -13,16 +14,34 @@ namespace CharismaShop.Services
     public class OrderService : ServiceBase<Order>, IOrderService
     {
         private readonly IMapper _mapper;
-        public OrderService(IServiceProvider serviceProvider, 
-            IMapper mapper) : base(serviceProvider)
+        private readonly IProductService _productService;
+        public OrderService(IServiceProvider serviceProvider,
+            IMapper mapper,
+            IProductService productService) : base(serviceProvider)
         {
             _mapper = mapper;
+            _productService = productService;
         }
 
         public async Task CreateOrderAsync(OrderDto dto)
         {
             var order = _mapper.Map<Order>(dto);
+            await CalculateOrderPrice(order);
             await AddAsync(order);
+        }
+
+        async Task CalculateOrderPrice(Order order)
+        {
+            //TODO: need cache
+            var products = await _productService.GetAll()
+                .Where(a => order.Items.Select(a => a.ProductId).Distinct().ToArray().Contains(a.Id))
+                .ToDictionaryAsync(a => a.Id, a => a.Price);
+            decimal totalPrice = 0;
+            foreach (var item in order.Items)
+            {
+                totalPrice += products[item.ProductId] * (decimal)item.Quantity;
+            }
+            order.Price = totalPrice;
         }
     }
 }
